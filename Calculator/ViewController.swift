f4d83971bec3caf71976fc0ca158f633c2531bd5
yk <yk1203.i@docomo.ne.jp>
2016-02-12 00:09:38 +0900

    a

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var number: Int = 0
+    var number2: Int = 0
+    var operation: Int = 0
+    
+    @IBAction func select3(){
+        number = 3
+        label.text = String(number)
+    }
+
+    @IBAction func select4(){
+        number = 4
+        label.text = String(number)
+    }
+    
+    @IBAction func plus(){
+        label.text = "0"
+        operation = 1
+        number2 = number
+        number = 0
+    }
+
+    @IBAction func equal(){
+        if operation == 1 {
+            label.text = String(number + number2)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
